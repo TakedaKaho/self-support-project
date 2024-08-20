9bd506c70ffae6624c1756df35a673cb439fbbdc
TakedaKaho <tktk.a3104c@gmail.com>
2024-08-20 13:08:21 +0000

    userコントローラー・ルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,23 @@ Rails.application.routes.draw do
  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
  sessions: "admin/sessions"
  }
+ 
+ 
+ # 管理者用ユーザー管理
+  namespace :admin do
+    resources :users, only: [:index, :show, :edit, :update]
+  end
+
+  # 顧客用ユーザー管理
+  scope module: :public do
+     resources :users, only: [:show, :edit, :update] do
+      collection do
+        get 'check'
+        patch 'out'
+        #上記で任意のアクションのルーティング
+      end
+     end 
+     
+  end
+  
 end
