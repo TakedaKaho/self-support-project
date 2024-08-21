a3fb540ca3e26806dcb8c81c1a6b428c50d8fac3
TakedaKaho <tktk.a3104c@gmail.com>
2024-08-21 04:54:20 +0000

    user/taskコントローラー

---
+++ app/controllers/public/tasks_controller.rb
@@ -0,0 +1,17 @@
+class Public::TasksController < ApplicationController
+  def new
+  end 
+  
+  def create
+  end 
+  
+  def index
+  end 
+  
+  def show
+  end 
+  
+  def update_status
+  end 
+  
+end
