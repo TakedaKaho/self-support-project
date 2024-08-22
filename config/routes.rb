5e7b946bcaee53c20f91a28535161ba97224459f
TakedaKaho <tktk.a3104c@gmail.com>
2024-08-22 03:52:11 +0000

    topページ作成・taskとtopのルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,33 +1,36 @@
 Rails.application.routes.draw do
  # 顧客用
- # URL /customers/sign_in ...
   devise_for :users,skip: [:passwords], controllers: {
   registrations: "public/registrations",
   sessions: 'public/sessions'
  }
 
  # 管理者用
- # URL /admin/sign_in ...
  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
  sessions: "admin/sessions"
  }
  
- 
  # 管理者用ユーザー管理
   namespace :admin do
+   get '/', to: 'homes#top'
     resources :users, only: [:index, :show, :edit, :update]
   end
 
   # 顧客用ユーザー管理
   scope module: :public do
+   root :to =>"homes#top"
      resources :users, only: [:show, :edit, :update] do
-      collection do
+      member do
         get 'check'
         patch 'out'
-        #上記で任意のアクションのルーティング
       end
      end 
      
+     resources :tasks,only:[:new, :index, :show] do
+      member do
+       patch 'update_status'
+      end
+     end
+     
   end
-  
 end
