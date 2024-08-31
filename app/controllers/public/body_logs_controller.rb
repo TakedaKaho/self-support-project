897a87f5126c905466960be3f48bd80e611bb869
TakedaKaho <tktk.a3104c@gmail.com>
2024-08-31 07:45:40 +0000

    記録日追加・flashメッセージ追加

--- app/controllers/public/body_logs_controller.rb
+++ app/controllers/public/body_logs_controller.rb
@@ -7,7 +7,7 @@ class Public::BodyLogsController < ApplicationController
  def create
   @body_log = current_user.body_logs.new(body_log_params)
   if @body_log.save
-   flash[:notice]="今日のBodyLogを記録しました"
+   flash[:body_log_notice]="今日のBodyLogを記録しました"
    redirect_to body_logs_path
   else
    render :new
@@ -25,6 +25,10 @@ class Public::BodyLogsController < ApplicationController
  end 
  
  def destroy
+  @body_log = BodyLog.find(params[:id])
+  @body_log.destroy
+  flash[:body_log_destroy_notice]="記録を削除しました。"
+  redirect_to body_logs_path
  end 
  
  private
