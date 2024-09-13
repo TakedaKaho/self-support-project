2d90c04dcac6249128336a5245f661f95f9005e8
TakedaKaho <tktk.a3104c@gmail.com>
2024-09-13 12:55:33 +0000

    [運動記録]create

--- app/controllers/public/exercise_logs_controller.rb
+++ app/controllers/public/exercise_logs_controller.rb
@@ -5,6 +5,7 @@ class Public::ExerciseLogsController < ApplicationController
  end 
  
  def create
+  @exercise =ExerciseLog.new(exercise_log_params)
  end 
  
  def index
@@ -17,6 +18,12 @@ class Public::ExerciseLogsController < ApplicationController
  end 
  
  def destroy
+ end 
+ 
+ private
+ 
+ def exercise_log_params
+  params.require(:exercise_log).permit(:exercise_date, :exercise_type, :exercise_duration, :notes)
  end 
  
 end
